4b7ffb6d8977e5fd9aa6c535f7a2329ab1ddb9e5
Ashwini <salunkheashwini1998@gmail.com>
2022-12-30 17:08:26 +0530

    THIS IS FILE

---
+++ StopWatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class StopWatch
+    {
+        public static void StartStopwatch()
+        {
+
+            // Create new stopwatch
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Time Start
+            Console.WriteLine("Press Enter for start");
+            Console.ReadLine();
+            stopwatch.Start();
+
+            // Time Stop
+            Console.WriteLine("Press Enter for stop");
+            Console.ReadLine();
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed : {0}", stopwatch.Elapsed);
+        }
+    }
+}
