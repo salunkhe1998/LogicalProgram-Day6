152cebf7e3a6db964a6b5d528b7dd15fd19b6c0b
Ashwini <salunkheashwini1998@gmail.com>
2022-12-30 16:33:26 +0530

    THIS IS FILE

---
+++ Factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class Factorial
+    {
+
+        public static void CalculateFactorial()
+        {
+            int factorial = 1;
+            Console.WriteLine("Enter the Number N :");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("Factorial of " + N + " is : " + factorial);
+
+        }
+    }
+}
+Footer
