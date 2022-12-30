5e9f465e9cd1b5f3bb6f569954d6db5b58664a2b
Ashwini <salunkheashwini1998@gmail.com>
2022-12-30 15:03:10 +0530

    THIS IS FILE

---
+++ PerfectNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class PerfectNumber
+    {
+        public static void CheckPerfectNumber()
+        {
+            int sum = 0;
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine($"Factors of {num} is : " + sum);
+
+
+                }
+            }
+
+            if (sum == num)
+            {
+                Console.WriteLine($"{num} is Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is Not a Perfect Number");
+            }
+
+
+
+        }
+    }
+}
