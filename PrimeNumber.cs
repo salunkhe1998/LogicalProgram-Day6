17f00b686d44efc4b506b59d52d517dd95215a17
Ashwini <salunkheashwini1998@gmail.com>
2022-12-30 16:18:50 +0530

    THIS IS FILE

---
+++ PrimeNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LogicalProgram
+{
+    public class PrimeNo
+    {
+        public static void CheckPrimeNumber()
+        {
+            Console.WriteLine("Enter the Number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+
+                if (num % i == 0)
+                {
+                    count++;
+                }
+
+            }
+
+            if (count == 2)
+            {
+                Console.WriteLine($"{num} is Prime Number");
+
+            }
+
+            else
+
+            {
+                Console.WriteLine($"{num} is Not a Prime Number");
+            }
+
+        }
+    }
+}
