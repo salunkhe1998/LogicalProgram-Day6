7a92c33d9b4a3834608fde7d77839cf5e9891eb4
Ashwini <salunkheashwini1998@gmail.com>
2022-12-31 16:28:46 +0530

    This is file

---
+++ CouponNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class Coupon
+    {
+        public static void CheckCoupon()
+        {
+            Console.WriteLine("how much time you want a unique random number :- ");
+            int N1 = Convert.ToInt32(Console.ReadLine());
+            var Num = Enumerable.Range(0, N1).ToArray();
+            var R = new Random();
+
+            for (int i = 0; i < Num.Length; i++)
+            {
+                int RAN = R.Next(Num.Length);
+                int vr = Num[RAN];
+                Num[RAN] = Num[i];
+                Num[i] = vr;
+            }
+            Console.Write("The unique random numbers are :-  ");
+            for (int i = 0; i < Num.Length; i++)
+                Console.Write(Num[i] + " ");
+        }
+    }
+}
